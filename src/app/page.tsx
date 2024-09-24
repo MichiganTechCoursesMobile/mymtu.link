2225f186b523444b77e8cb72eece9cb2750556c2
Luis Vervaet <lrvervae@mtu.edu>
2024-09-24 15:10:16 -0400

    DaisyUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import Button from "@mui/material/Button";
-
 export default function Home() {
-  return (
-    <div>
-      <Button variant="outlined" color="primary">
-        Outlined
-      </Button>
-    </div>
-  );
+  return <div></div>;
 }
