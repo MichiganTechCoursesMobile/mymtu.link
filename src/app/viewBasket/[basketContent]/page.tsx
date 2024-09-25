6b2fb765164f7b4110e8812dc2733bda293fbb8a
Luis Vervaet <lrvervae@mtu.edu>
2024-09-25 11:08:44 -0400

    Wow, I hate cache

--- src/app/viewBasket/[basketContent]/page.tsx
+++ src/app/viewBasket/[basketContent]/page.tsx
@@ -1,7 +1,8 @@
 // Example basket content: MTUANDROID:SEMESTER=SPRING-2024&CRNS=12345,67890&BASKET_NAME=Gabagool&NAME=
 
-import { useEffect, useState } from "react";
+import { cache, useEffect, useState } from "react";
 import { NextResponse } from "next/server";
+import { unstable_cache } from "next/cache";
 
 export default async function Page({
   params,
@@ -18,7 +19,6 @@ export default async function Page({
     basket.split("&").forEach((pair) => {
       const [key, value] = pair.split("=");
       basketMap.set(key, value);
-      console.log(key, value);
     });
   } else {
     console.log("Invalid basket");
@@ -31,19 +31,11 @@ export default async function Page({
 
   const getSectionData = `https://api.michigantechcourses.com/sections?semester=${semester}&year=${semesterYear}`;
 
-  const [data, setData] = useState(null);
+  const getSections = unstable_cache(async () =>
+    (await fetch(getSectionData)).json()
+  );
+  let data = await await getSections();
+  // console.log(data);
 
-  useEffect(() => {
-    // Function to fetch data
-    const fetchData = async () => {
-      const response = await fetch(getSectionData); // Replace with your API endpoint
-      const result = await response.json();
-      setData(result);
-    };
-
-    // Call the function
-    fetchData();
-  }, []); // Empty array ensures this effect runs once on mount
-
-  return <p>{data}</p>;
+  return <p>{}</p>;
 }
